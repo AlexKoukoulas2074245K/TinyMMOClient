83a7fd0d39ade8e9c5cb521b455f3043a9d2cde0
Alex Koukoulas <alexios.koukoulas.2013@gmail.com>
2025-03-21 11:38:55 +0000

    Added board population constraints for Wild & Scatter symbols & unit tests

--- source_test/net_common/BoardTest.cpp
+++ source_test/net_common/BoardTest.cpp
@@ -7,6 +7,7 @@
 
 #include <gtest/gtest.h>
 #include <net_common/Board.h>
+#include <engine/utils/MathUtils.h>
 
 ///------------------------------------------------------------------------------------------------
 
@@ -15,6 +16,57 @@ TEST(BoardTest, TestBasicBoardAccessors)
     slots::Board b;
     b.SetBoardSymbol(0, 0, slots::SymbolType::CHICKEN);
     ASSERT_EQ(b.GetBoardSymbol(0, 0), slots::SymbolType::CHICKEN);
+}
+
+///------------------------------------------------------------------------------------------------
+
+TEST(BoardTest, TestComplexSymbolsNotAppearingInRandomBoardPopulation)
+{
+    slots::Board b;
+    
+    bool foundComplexSymbol = false;
+    for (int i = 0; i < 1000000 && !foundComplexSymbol; ++i)
+    {
+        b.PopulateBoardForSpin(math::RandomInt());
+        
+        for (int reelIndex = 0; reelIndex < slots::BOARD_COLS; ++reelIndex)
+        {
+            if (b.GetSymbolCountInReel(reelIndex, slots::SymbolType::STRAWBERRY_CAKE) > 0 ||
+                b.GetSymbolCountInReel(reelIndex, slots::SymbolType::CHOCOLATE_CAKE) > 0 ||
+                b.GetSymbolCountInReel(reelIndex, slots::SymbolType::ROAST_CHICKEN) > 0)
+            {
+                foundComplexSymbol = true;
+                break;
+            }
+        }
+    }
+    
+    ASSERT_EQ(foundComplexSymbol, false);
+}
+
+///------------------------------------------------------------------------------------------------
+
+TEST(BoardTest, TestWildAndScatterAppearOnlyOnceInEachReelInRandomBoardPopulation)
+{
+    slots::Board b;
+    
+    bool foundDuplicateWildOrScatterInReel = false;
+    for (int i = 0; i < 1000000 && !foundDuplicateWildOrScatterInReel; ++i)
+    {
+        b.PopulateBoardForSpin(math::RandomInt());
+        
+        for (int reelIndex = 0; reelIndex < slots::BOARD_COLS; ++reelIndex)
+        {
+            if (b.GetSymbolCountInReel(reelIndex, slots::SymbolType::WILD) > 1 ||
+                b.GetSymbolCountInReel(reelIndex, slots::SymbolType::SCATTER) > 1)
+            {
+                foundDuplicateWildOrScatterInReel = true;
+                break;
+            }
+        }
+    }
+    
+    ASSERT_EQ(foundDuplicateWildOrScatterInReel, false);
 }
 
 ///------------------------------------------------------------------------------------------------
